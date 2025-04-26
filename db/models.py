c36fceb57ad0a0ae0000a4625ba0593353d29b12
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-04-26 21:25:39 +0100

    v4

--- db/models.py
+++ db/models.py
@@ -21,7 +21,7 @@ class Guild(models.Model):
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
-    #bio - a CharField with a maximum length of 255 characters.
+    # (!)bio - a CharField with a maximum length of 255 characters.
     # It stores a short description provided by a user about himself/herself.
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
