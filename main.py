bda6d904e845c22f3d1dca96de67eb6f6611cbc0
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-04-26 21:17:05 +0100

    v2

--- main.py
+++ main.py
@@ -12,32 +12,32 @@ def main() -> None:
     for nickname, player in players.items():
 
         race, created = Race.objects.get_or_create(
-            name = player["race"]["name"],
-            description = player["race"]["description"],
+            name=player["race"]["name"],
+            description=player["race"]["description"],
         )
 
         if created:
             for skill in player["race"]["skills"]:
                 Skill.objects.get_or_create(
-                    name = skill["name"],
-                    bonus = skill["bonus"],
-                    race = race,
+                    name=skill["name"],
+                    bonus=skill["bonus"],
+                    race=race,
                 )
 
         if player["guild"]:
             guild, _ = Guild.objects.get_or_create(
-                name = player["guild"]["name"],
-                description = player["guild"]["description"],
+                name=player["guild"]["name"],
+                description=player["guild"]["description"],
             )
         else:
             guild = None
 
         Player.objects.create(
-            nickname = nickname,
-            email = player["email"],
-            bio = player["bio"],
-            race = race,
-            guild = guild
+            nickname=nickname,
+            email=player["email"],
+            bio=player["bio"],
+            race=race,
+            guild=guild
         )
 
 
